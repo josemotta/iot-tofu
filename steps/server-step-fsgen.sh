6e55b87bc210965288281cd98913f2ce997fff4d
Jose Motta <josemotta@bampli.com>
2023-08-01 13:03:31 -0300

    add steps to gen/del fs

---
+++ steps/server-step-fsgen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ $1 == "" ]; then
+  echo Missing client id
+  exit 1
+fi
+export CLIENT_ID=$1
+
+# Generate a copy from current fs to $CLIENT_ID
+sudo mkdir -p /nfs/$CLIENT_ID
+sudo apt install rsync
+sudo rsync -xa --progress --exclude /nfs / /nfs/$CLIENT_ID
+
+# Regenerate SSH host keys on the CLIENT_ID filesystem
+# by chrooting into it
+cd /nfs/$CLIENT_ID
+sudo mount --bind /dev dev
+sudo mount --bind /sys sys
+sudo mount --bind /proc proc
+sudo chroot .
+rm /etc/ssh/ssh_host_*
+dpkg-reconfigure openssh-server
+exit
+sudo umount dev sys proc
+
+echo Generated fs for $CLIENT_ID
