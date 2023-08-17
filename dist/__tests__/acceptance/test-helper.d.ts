a599f3e79e6c63e31379520f6df02203ae932423
Jose Motta <josemotta@bampli.com>
2023-08-17 15:09:00 -0300

    fix ignored files

--- dist/__tests__/acceptance/test-helper.d.ts
+++
@@ -1,7 +0,0 @@
-import { BackApplication } from '../..';
-import { Client } from '@loopback/testlab';
-export declare function setupApplication(): Promise<AppWithClient>;
-export interface AppWithClient {
-    app: BackApplication;
-    client: Client;
-}
