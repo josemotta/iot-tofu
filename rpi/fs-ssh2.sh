2632c2c0e046670e06988f9ba72ec1a1b941d337
Jose Motta <josemotta@bampli.com>
2023-10-19 13:00:12 -0300

    add fs-ssh2 to fill known_hosts & authorized_keys

--- rpi/fs-ssh2.sh
+++ rpi/fs-ssh2.sh
@@ -8,8 +8,22 @@ export BASE_FS=$1
 
 KEYS='/etc/ssh/ssh_host_*'
 
+#
+# known_hosts
+#
+
 # copy boot server keys to rpi
 # $KEYS ---> $BASE_FS/etc/ssh/known_hosts
 
 # copy rpi keys to boot server
 # $BASE_FS/$KEYS ---> ~/.ssh/known_hosts
+
+#
+# authorized_keys
+#
+
+# copy boot server keys to rpi
+# $KEYS ---> $BASE_FS/home/$USER/.ssh/authorized_keys
+
+# copy rpi keys to boot server
+# $BASE_FS/$KEYS ---> ~/.ssh/authorized_keys
