c00f9c825a7bb20ac45c6a99bc3be6f9afb9417d
Jose Motta <josemotta@bampli.com>
2023-10-20 12:58:58 -0300

    rev ssh gen

--- rpi/fs-ssh2.sh
+++ rpi/fs-ssh2.sh
@@ -7,59 +7,63 @@ fi
 export BASE_FS=$1
 
 OWNER=$(<$BASE_FS/boot/owner)
-KEY=ssh_host_rsa_key.pub
 
-RPI_KEY=$BASE_FS/etc/ssh/$KEY
-RPI_OWNER_HOME=$BASE_FS/home/$OWNER
-RPI_AUTHORIZED_KEYS=$RPI_OWNER_HOME/.ssh/authorized_keys
+RPI_USR_HOME=$BASE_FS/home/$OWNER
+RPI_USR_AUTHORIZED_KEYS=$RPI_USR_HOME/.ssh/authorized_keys
+RPI_USR_KNOWN_HOSTS=$RPI_USR_HOME/.ssh/known_hosts
+RPI_USR_KEY=$RPI_USR_HOME/.ssh/id_rsa.pub
+RPI_SYS_KEY=$BASE_FS/etc/ssh/ssh_host_rsa_key.pub
+RPI_SYS_KNOWN_HOSTS=$BASE_FS/etc/ssh/known_hosts
 
-SRV_KEY=/etc/ssh/$KEY
-SRV_OWNER_HOME=/home/$OWNER
-SRV_AUTHORIZED_KEYS=$SRV_OWNER_HOME/.ssh/authorized_keys
-SRV_KNOWN_HOSTS=$SRV_OWNER_HOME/.ssh/known_hosts
+SRV_USR_HOME=/home/$OWNER
+SRV_USR_AUTHORIZED_KEYS=$SRV_USR_HOME/.ssh/authorized_keys
+SRV_USR_KNOWN_HOSTS=$SRV_USR_HOME/.ssh/known_hosts
+SRV_USR_KEY=$SRV_USR_HOME/.ssh/id_rsa.pub
+SRV_SYS_KEY=/etc/ssh/ssh_host_rsa_key.pub
+SRV_SYS_KNOWN_HOSTS=/etc/ssh/known_hosts
 
 #
 # known_hosts
 #
 
 # rpi system-wide known_hosts: boot server -> rpi
-# $SRV_KEY ---> $BASE_FS/etc/ssh/known_hosts
-touch $BASE_FS/etc/ssh/known_hosts
-chmod 644 $BASE_FS/etc/ssh/known_hosts
-ssh-keygen -l -E md5 -f $SRV_KEY >> $BASE_FS/etc/ssh/known_hosts
+# $SRV_SYS_KEY ---> $BASE_FS/etc/ssh/known_hosts
+touch $RPI_SYS_KNOWN_HOSTS
+chmod 644 $RPI_SYS_KNOWN_HOSTS
+ssh-keygen -l -E md5 -f $SRV_SYS_KEY >> $RPI_SYS_KNOWN_HOSTS
 
 # boot server system-wide known_hosts: rpi -> boot server
-# $RPI_KEY ---> /etc/ssh/known_hosts
-if [ ! -d /etc/ssh/known_hosts ]; then
-  touch /etc/ssh/known_hosts
-  chmod 644 /etc/ssh/known_hosts
+# $RPI_SYS_KEY ---> /etc/ssh/known_hosts
+if [ ! -d $SRV_SYS_KNOWN_HOSTS ]; then
+  touch $SRV_SYS_KNOWN_HOSTS
+  chmod 644 $SRV_SYS_KNOWN_HOSTS
 fi
-ssh-keygen -l -E md5 -f $RPI_KEY >> /etc/ssh/known_hosts
+ssh-keygen -l -E md5 -f $RPI_SYS_KEY >> $SRV_SYS_KNOWN_HOSTS
 
 # boot server local-client known_hosts: rpi -> boot server
-# $RPI_KEY ---> ~/.ssh/known_hosts
-if [ ! -d $SRV_KNOWN_HOSTS ]; then
-  touch $SRV_KNOWN_HOSTS
-  chmod 644 $SRV_KNOWN_HOSTS
+# $RPI_SYS_KEY ---> ~/.ssh/known_hosts
+if [ ! -d $SRV_USR_KNOWN_HOSTS ]; then
+  touch $SRV_USR_KNOWN_HOSTS
+  chmod 644 $SRV_USR_KNOWN_HOSTS
 fi
-ssh-keygen -l -E md5 -f $RPI_KEY >> $SRV_KNOWN_HOSTS
+ssh-keygen -l -E md5 -f $RPI_SYS_KEY >> $SRV_USR_KNOWN_HOSTS
 
 #
 # authorized_keys
 #
 
 # boot server -> rpi
-# $SRV_KEY ---> $BASE_FS/home/$OWNER/.ssh/authorized_keys
-mkdir -p $RPI_OWNER_HOME/.ssh
-chmod 700 $RPI_OWNER_HOME/.ssh
-touch $RPI_AUTHORIZED_KEYS
-chmod 600 $RPI_AUTHORIZED_KEYS
-ssh-keygen -l -E md5 -f $SRV_KEY >> $RPI_AUTHORIZED_KEYS
+# $SRV_SYS_KEY ---> $BASE_FS/home/$OWNER/.ssh/authorized_keys
+mkdir -p $RPI_USR_HOME/.ssh
+chmod 700 $RPI_USR_HOME/.ssh
+touch $RPI_USR_AUTHORIZED_KEYS
+chmod 600 $RPI_USR_AUTHORIZED_KEYS
+ssh-keygen -l -E md5 -f $SRV_SYS_KEY >> $RPI_USR_AUTHORIZED_KEYS
 
 # rpi -> boot server
-# $RPI_KEY ---> ~/.ssh/authorized_keys
-if [ ! -d $SRV_AUTHORIZED_KEYS ]; then
-  touch $SRV_AUTHORIZED_KEYS
-  chmod 644 $SRV_AUTHORIZED_KEYS
+# $RPI_SYS_KEY ---> ~/.ssh/authorized_keys
+if [ ! -d $SRV_USR_AUTHORIZED_KEYS ]; then
+  touch $SRV_USR_AUTHORIZED_KEYS
+  chmod 644 $SRV_USR_AUTHORIZED_KEYS
 fi
-ssh-keygen -l -E md5 -f $RPI_KEY >> $SRV_AUTHORIZED_KEYS
+ssh-keygen -l -E md5 -f $RPI_SYS_KEY >> $SRV_USR_AUTHORIZED_KEYS
