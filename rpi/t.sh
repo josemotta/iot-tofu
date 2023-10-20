bee24e1da70196f8a2b32f5b47ba0f8d11292907
Jose Motta <josemotta@bampli.com>
2023-10-20 18:06:12 -0300

    add checkup test

---
+++ rpi/t.sh
@@ -0,0 +1,22 @@
+echo '====srv===================================='
+echo '-------- ~/.ssh ---------------------------'
+ls -l ~/.ssh
+echo '-------- /etc/ssh -------------------------'
+ls -l /etc/ssh
+echo '-------- srv-usr known-hosts --------------'
+cat ~/.ssh/known_hosts
+echo '--------         authorized_keys ----------'
+cat ~/.ssh/authorized_keys
+echo '-------- srv-sys known-hosts --------------'
+cat /etc/ssh/known_hosts
+echo '====rpi===================================='
+echo '-------- ~/.ssh ---------------------------'
+ls -l /nfs/9f55bbfd/home/jo/.ssh
+echo '-------- /etc/ssh -------------------------'
+ls -l /nfs/9f55bbfd/etc/ssh
+echo '-------- rpi-usr known-hosts --------------'
+cat /nfs/9f55bbfd/home/jo/.ssh/known_hosts
+echo '--------         authorized_keys ----------'
+cat /nfs/9f55bbfd/home/jo/.ssh/authorized_keys
+echo '-------- rpi-sys known-hosts --------------'
+cat /nfs/9f55bbfd/etc/ssh/known_hosts
