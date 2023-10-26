6a08109c13421dde353f850a4d1a6c266f559a78
Jose Motta <josemotta@bampli.com>
2023-10-26 19:33:05 -0300

    single file for known_hosts & authorized_keys

--- rpi/fs-ssh2.sh
+++ rpi/fs-ssh2.sh
@@ -101,7 +101,7 @@ EOF
 cat << EOF | sudo tee $RPI_SSHD_CONFIG/config.conf
 HostbasedAuthentication yes
 PubkeyAuthentication no
-UseDNS yes
+UseDNS no
 IgnoreRhosts no
 AcceptEnv -LC_* -LANG*
 Match User $OWNER
@@ -118,7 +118,7 @@ EOF
 cat << EOF | sudo tee $SRV_SSHD_CONFIG/config.conf
 HostbasedAuthentication yes
 PubkeyAuthentication no
-UseDNS yes
+UseDNS no
 IgnoreRhosts no
 AcceptEnv -LC_* -LANG*
 Match User $OWNER
