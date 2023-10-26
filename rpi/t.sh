a2e554e38da2df7855d3304797188cecd80872bd
Jose Motta <josemotta@bampli.com>
2023-10-26 12:51:32 -0300

    single file for known_hosts & authorized_keys

--- rpi/t.sh
+++ rpi/t.sh
@@ -8,11 +8,11 @@ cat ~/.ssh/known_hosts
 echo '-------- k e y s --------------------------'
 ssh-keygen -lf ~/.ssh/known_hosts
 echo '--------         authorized_keys ----------'
-# cat ~/.ssh/authorized_keys
+cat ~/.ssh/authorized_keys
 echo '-------- k e y s --------------------------'
 sudo ssh-keygen -lf ~/.ssh/authorized_keys
 echo '-------- /etc/ssh/known-hosts -------------'
-# cat /etc/ssh/ssh_known_hosts
+cat /etc/ssh/ssh_known_hosts
 echo '-------- k e y s --------------------------'
 sudo ssh-keygen -lf /etc/ssh/ssh_known_hosts
 echo '-------- rsa dsa ecdsa ed25519 -----------'
@@ -27,11 +27,11 @@ ls -l /nfs/9f55bbfd/home/jo/.ssh
 echo '-------- /etc/ssh -------------------------'
 ls -l /nfs/9f55bbfd/etc/ssh
 echo '-------- ~/.ssh/known-hosts ---------------'
-# cat /nfs/9f55bbfd/home/jo/.ssh/known_hosts
+cat /nfs/9f55bbfd/home/jo/.ssh/known_hosts
 echo '-------- k e y s --------------------------'
 sudo ssh-keygen -lf /nfs/9f55bbfd/home/jo/.ssh/known_hosts
 echo '--------         authorized_keys ----------'
-# cat /nfs/9f55bbfd/home/jo/.ssh/authorized_keys
+cat /nfs/9f55bbfd/home/jo/.ssh/authorized_keys
 echo '-------- k e y s --------------------------'
 sudo ssh-keygen -lf /nfs/9f55bbfd/home/jo/.ssh/authorized_keys
 echo '-------- /etc/ssh/known-hosts -------------'
@@ -50,11 +50,11 @@ ls -l /nfs/a10cd2e5/home/jo/.ssh
 echo '-------- /etc/ssh -------------------------'
 ls -l /nfs/a10cd2e5/etc/ssh
 echo '-------- ~/.ssh/known-hosts ---------------'
-# cat /nfs/a10cd2e5/home/jo/.ssh/known_hosts
+cat /nfs/a10cd2e5/home/jo/.ssh/known_hosts
 echo '-------- k e y s --------------------------'
 sudo ssh-keygen -lf /nfs/a10cd2e5/home/jo/.ssh/known_hosts
 echo '--------         authorized_keys ----------'
-# cat /nfs/a10cd2e5/home/jo/.ssh/authorized_keys
+cat /nfs/a10cd2e5/home/jo/.ssh/authorized_keys
 echo '-------- k e y s --------------------------'
 sudo ssh-keygen -lf /nfs/a10cd2e5/home/jo/.ssh/authorized_keys
 echo '-------- /etc/ssh/known-hosts -------------'
