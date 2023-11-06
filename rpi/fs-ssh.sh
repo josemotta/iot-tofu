39cfa5aafc288ca67e617e5f57dc5f3cf34a8780
Jose Motta <josemotta@bampli.com>
2023-11-06 13:41:29 -0300

    config locale

--- rpi/fs-ssh.sh
+++ rpi/fs-ssh.sh
@@ -10,9 +10,16 @@ dpkg-reconfigure openssh-server
 # https://stackoverflow.com/questions/43235179/how-to-execute-ssh-keygen-without-prompt
 ssh-keygen -q -t rsa -N '' <<< $'\ny' >/dev/null 2>&1
 
-echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+# Config locale
+cat << EOF | sudo tee /etc/default/locale
+LANG=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+LANGUAGE=en_US.UTF-8
+EOF
+
+# echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 # echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+# echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 # add aliases to default .bashrc
 cat << EOF >> /etc/skel/.bashrc
