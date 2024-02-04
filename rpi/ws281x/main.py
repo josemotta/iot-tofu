c1db4f54d68482aac0144eaba2cc367005dd68c6
Jose Motta <josemotta@bampli.com>
2024-02-04 11:47:54 -0300

    change lib to NeoPixel

--- rpi/ws281x/main.py
+++ rpi/ws281x/main.py
@@ -7,9 +7,11 @@ from strip_manager import StripManager
 app = Flask(__name__)
 strip_manager = StripManager.default()
 
+
 @app.route('/')
 def hello_world():
     return 'Back-end ws281x v0.1'
+
 
 @app.route("/led-strip")
 def status():
@@ -19,7 +21,7 @@ def status():
 @app.route("/led-strip", methods=['POST'])
 def toggle():
     body = request.get_json()
-    print(json.dumps(body,indent=2))
+    print(json.dumps(body, indent=2))
     hue = 0.0
     sat = 0.0
     brightness = 255
@@ -41,36 +43,36 @@ def toggle():
 
     return jsonify(strip_manager.status())
 
-@app.route("/led-strip/fill", methods=['POST'])
-def toggle():
-    body = request.get_json()
-    print(json.dumps(body,indent=2))
-    hue = 0.0
-    sat = 0.0
-    first = 0
-    size = 0
-    brightness = 255
-    if first in body:
-        first = int(body['first'])
-    if size in body:
-        size = int(body['size'])
-    if 'hue' in body:
-        hue = body['hue'] / 360.0
-    if 'sat' in body:
-        sat = body['sat'] / 100.0
-    if 'brightness' in body:
-        brightness = int(body["brightness"])
-    rgb = colorsys.hsv_to_rgb(hue, sat, 1.0)
-    rgb_r = int(rgb[0] * 255)
-    rgb_g = int(rgb[1] * 255)
-    rgb_b = int(rgb[2] * 255)
-    if body['on']:
-        print(f'Fill pixels with {rgb_r} {rgb_g} {rgb_b}')
-        strip_manager.fill(rgb_r, rgb_g, rgb_b, first, size, brightness)
-    # else:
-    #     strip_manager.clear()
+# @app.route("/led-strip/fill", methods=['POST'])
+# def toggle():
+#     body = request.get_json()
+#     print(json.dumps(body,indent=2))
+#     hue = 0.0
+#     sat = 0.0
+#     first = 0
+#     size = 0
+#     brightness = 255
+#     if first in body:
+#         first = int(body['first'])
+#     if size in body:
+#         size = int(body['size'])
+#     if 'hue' in body:
+#         hue = body['hue'] / 360.0
+#     if 'sat' in body:
+#         sat = body['sat'] / 100.0
+#     if 'brightness' in body:
+#         brightness = int(body["brightness"])
+#     rgb = colorsys.hsv_to_rgb(hue, sat, 1.0)
+#     rgb_r = int(rgb[0] * 255)
+#     rgb_g = int(rgb[1] * 255)
+#     rgb_b = int(rgb[2] * 255)
+#     if body['on']:
+#         print(f'Fill pixels with {rgb_r} {rgb_g} {rgb_b}')
+#         strip_manager.fill(rgb_r, rgb_g, rgb_b, first, size, brightness)
+#     # else:
+#     #     strip_manager.clear()
 
-    return jsonify(strip_manager.status())
+#     return jsonify(strip_manager.status())
 
 
 if __name__ == '__main__':
