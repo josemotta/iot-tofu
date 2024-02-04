1bfc5c31dea1b3855f9f2edee4dc5a3c42290a0b
Jose Motta <josemotta@bampli.com>
2024-02-04 12:49:37 -0300

    simplify web server

--- rpi/ws281x/main.py
+++ rpi/ws281x/main.py
@@ -1,11 +1,27 @@
 import colorsys
 import json
 from flask import Flask, jsonify, request
+import time
+import board
+import neopixel
 
-from strip_manager import StripManager
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 20
+
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+ORDER = neopixel.RGB
+
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+
+status = False
 
 app = Flask(__name__)
-strip_manager = StripManager.default()
 
 
 @app.route('/')
@@ -15,33 +31,33 @@ def hello_world():
 
 @app.route("/led-strip")
 def status():
-    return jsonify(strip_manager.status())
+    return jsonify(status)
 
 
-@app.route("/led-strip", methods=['POST'])
-def toggle():
-    body = request.get_json()
-    print(json.dumps(body, indent=2))
-    hue = 0.0
-    sat = 0.0
-    brightness = 255
-    if 'hue' in body:
-        hue = body['hue'] / 360.0
-    if 'sat' in body:
-        sat = body['sat'] / 100.0
-    if 'brightness' in body:
-        brightness = int(body["brightness"])
-    rgb = colorsys.hsv_to_rgb(hue, sat, 1.0)
-    rgb_r = int(rgb[0] * 255)
-    rgb_g = int(rgb[1] * 255)
-    rgb_b = int(rgb[2] * 255)
-    if body['on']:
-        print(f'Setting colour to {rgb_r} {rgb_g} {rgb_b}')
-        strip_manager.solid_color(rgb_r, rgb_g, rgb_b, brightness)
-    else:
-        strip_manager.clear()
+# @app.route("/led-strip", methods=['POST'])
+# def toggle():
+#     body = request.get_json()
+#     print(json.dumps(body, indent=2))
+#     hue = 0.0
+#     sat = 0.0
+#     brightness = 255
+#     if 'hue' in body:
+#         hue = body['hue'] / 360.0
+#     if 'sat' in body:
+#         sat = body['sat'] / 100.0
+#     if 'brightness' in body:
+#         brightness = int(body["brightness"])
+#     rgb = colorsys.hsv_to_rgb(hue, sat, 1.0)
+#     rgb_r = int(rgb[0] * 255)
+#     rgb_g = int(rgb[1] * 255)
+#     rgb_b = int(rgb[2] * 255)
+#     if body['on']:
+#         print(f'Setting colour to {rgb_r} {rgb_g} {rgb_b}')
+#         strip_manager.solid_color(rgb_r, rgb_g, rgb_b, brightness)
+#     else:
+#         strip_manager.clear()
 
-    return jsonify(strip_manager.status())
+#     return jsonify(strip_manager.status())
 
 # @app.route("/led-strip/fill", methods=['POST'])
 # def toggle():
