49e3db21e16c20b16526266cd70c39502c2cb8fb
Jose Motta <josemotta@bampli.com>
2025-08-28 11:12:57 -0300

    change picture name to fit graph

--- rpi/vl53l1x/ha/install-ha.sh
+++ rpi/vl53l1x/ha/install-ha.sh
@@ -29,6 +29,26 @@ if [ ! -d /home/$USER/.docker/ha/config/configuration.yaml ]; then
     exit
 fi
 
+
+echo -n "hello" >> <filename>
+
+cat << EOF | sudo tee /etc/exports
+rest:
+  resource: 'http://127.0.0.1:5000/test'
+  scan_interval: 2
+  sensor:
+    - name: 'distance'
+      value_template: '{{ value_json['distance'] | round(1) }}'
+      device_class: distance
+      unit_of_measurement: 'mm'
+    - name: 'chip'
+      value_template: '{{ value_json['chip'] }}'
+      device_class: specs
+    - name: 'version'
+      value_template: '{{ value_json['version'] }}'
+      device_class: specs
+EOF
+
 echo "---- Configuration"
 cp --remove-destination configuration.yaml /home/$USER/.docker/ha/config
 echo "Added rest configuration for vl53l1x distance sensor"
