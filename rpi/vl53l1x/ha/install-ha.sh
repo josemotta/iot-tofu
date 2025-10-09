193849f177d89a03ba448075cb52cc4f23ce4e61
Jose Motta <josemotta@bampli.com>
2025-10-09 18:30:04 -0300

    review ha install & setup for raw sensors data

--- rpi/vl53l1x/ha/install-ha.sh
+++ rpi/vl53l1x/ha/install-ha.sh
@@ -22,24 +22,33 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-if [ ! -d /home/$USER/.docker/ha/config/configuration.yaml ]; then
+if [ ! -d /home/$USER/.docker/ha/config/ ]; then
     echo "Missing expected Homeassistant configuration at ./docker/ha/config"
     exit
 fi
 
-cat << EOF | tee -a /home/$USER/.docker/ha/config/configuration.yaml
+cp ./configuration.yaml /home/$USER/.docker/ha/config/configuration.yaml
+cp ./secrets.yaml /home/$USER/.docker/ha/config/secrets.yaml
+cp ./sensors.yaml /home/$USER/.docker/ha/config/sensors.yaml
 
-rest:
-  resource: 'http://127.0.0.1:5000/test'
-  scan_interval: 2
-  sensor:
-    - name: 'distance'
-      value_template: '{{ value_json['distance'] | round(1) }}'
-      device_class: distance
-      unit_of_measurement: 'mm'
-EOF
+# if [ ! -d /home/$USER/.docker/ha/config/configuration.yaml ]; then
+#     echo "Missing expected Homeassistant configuration at ./docker/ha/config"
+#     exit
+# fi
 
-echo "Appended homeassistant configuration for vl53l1x distance sensor"
+# cat << EOF | tee -a /home/$USER/.docker/ha/config/configuration.yaml
+
+# rest:
+#   resource: 'http://127.0.0.1:5000/test'
+#   scan_interval: 2
+#   sensor:
+#     - name: 'distance'
+#       value_template: '{{ value_json['distance'] | round(1) }}'
+#       device_class: distance
+#       unit_of_measurement: 'mm'
+# EOF
+
+echo "Appended homeassistant configuration for VL53L1X Time-of-Flight (ToF) laser-ranging sensor."
 
 echo "The directory & configuration file are shown below:"
 ls -l /home/$USER/.docker/ha/config/
